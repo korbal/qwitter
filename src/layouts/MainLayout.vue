1c3cbb5f2d7b82b5b067573e681bb4df948f7d86
korbal <balint.j.korosi@gmail.com>
2022-03-18 16:12:32 +0100

    left drawer done

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -8,14 +8,53 @@
         <q-toolbar-title class="text-weight-bold">
        
           Qwitter
-        </q-toolbar-title>
+        </q-toolbar-title>  
 
        
       </q-toolbar>
     </q-header>
 
-    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
-      <!-- drawer content -->
+    <q-drawer
+      show-if-above
+      v-model="leftDrawerOpen"
+      side="left"
+      bordered
+      :width="283">
+
+      <q-icon
+      class="q-pa-md"
+      name="fas fa-dove"
+      size="lg"
+      color="primary" />
+
+    <q-list >
+      <q-item
+        to="/"
+        clickable
+        v-ripple>
+        <q-item-section avatar>
+          <q-icon  name="home" size="md" />
+        </q-item-section>
+
+        <q-item-section class="text-h6 text-weight-bold">Home</q-item-section>
+      </q-item>
+
+      <q-item
+        to="/about"
+        clickable
+        v-ripple>
+        <q-item-section avatar>
+          <q-icon  name="help" size="md" />
+        </q-item-section>
+
+        <q-item-section class="text-h6 text-weight-bold">About</q-item-section>
+      </q-item>
+    </q-list>
+
+    
+
+
+
     </q-drawer>
 
     <q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered>
