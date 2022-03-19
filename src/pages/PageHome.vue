deaaf7bf13babd8bd08766ad7527b8a9579552db
korbal <balint.j.korosi@gmail.com>
2022-03-19 11:00:15 +0100

    tweet form done

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,6 +1,44 @@
 <template>
   <q-page>
-    <h5>Home</h5>
+    <div class="q-py-lg q-px-md row items-end q-col-gutter-md">
+
+      <div class="col">
+        <q-input
+        class="new-qweet"
+        bottom-slots
+        v-model="newQweetContent"
+        placeholder="What's happening?"
+        counter
+        maxlength="280"
+        autofocus
+        autogrow
+        >
+        <template v-slot:before>
+          <q-avatar size="xl">
+            <img src="https://cdn.quasar.dev/img/avatar5.jpg">
+          </q-avatar>
+        </template>
+      
+        
+      </q-input>
+      </div>
+      <div class="col col-shrink">
+         <q-btn
+         class="q-mb-lg"
+            unelevated
+            rounded
+            color="primary"
+            label="Qweet"
+            no-caps
+            :disable="!newQweetContent" />
+      </div>
+
+    </div>
+    <q-separator
+      size="10px"
+      color="grey-2"
+      class="divider" />
+
   </q-page>
 </template>
 
@@ -8,6 +46,21 @@
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'PageHome'
+  name: 'PageHome',
+  data(){
+    return {
+      newQweetContent:''
+    }
+  }
 })
 </script>
+<style lang="sass">
+  .new-qweet
+    textarea
+      font-size: 19px
+      line-height: 1.4 !important
+  .divider
+    border-top: 1px solid
+    border-bottom: 1px solid
+    border-color: grey-2
+</style>
