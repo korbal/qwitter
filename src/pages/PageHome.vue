3a680c4b003244b57e2ae92faba453111c0c0995
korbal <balint.j.korosi@gmail.com>
2022-03-19 11:25:34 +0100

    tweet design done

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -39,6 +39,60 @@
       color="grey-2"
       class="divider" />
 
+     <q-list>
+      <q-item class="q-py-md">
+        <q-item-section avatar top>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/img/avatar5.jpg">
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label class="text-subtitle1">
+            <strong>Danny Connel</strong>
+            <span class="text-grey-7"> @danny__connell</span>
+          </q-item-label>
+          <q-item-label class="qweet-content text-body1" >
+           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione numquam quisquam in tempore atque cum, 
+           
+           facere explicabo quis, modi nihil recusandae, architecto reiciendis consectetur quibusdam distinctio reprehenderit laudantium nisi molestias?
+          </q-item-label>
+          <div class="qweet-icons row justify-between q-mt-sm">
+            <q-btn
+              flat
+              round
+              color="grey"
+              icon="far fa-comment"
+              size="sm" />
+            <q-btn
+              flat
+              round
+              color="grey"
+              icon="fas fa-retweet"
+              size="sm" />
+            <q-btn
+              flat
+              round
+              color="grey"
+              icon="far fa-heart"
+              size="sm" />
+            <q-btn
+              flat
+              round
+              color="grey"
+              icon="fas fa-trash"
+              size="sm" />
+          </div>
+        </q-item-section>
+
+        <q-item-section side top>
+          1 min ago
+        </q-item-section>
+      </q-item>
+
+      
+    </q-list>
+
   </q-page>
 </template>
 
@@ -63,4 +117,8 @@ export default defineComponent({
     border-top: 1px solid
     border-bottom: 1px solid
     border-color: grey-2
+  .qweet-content
+    white-space:pre-line
+  .qweet-icons
+    margin-left: -5px
 </style>
